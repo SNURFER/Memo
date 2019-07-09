d60791dd3b735cd6d07d6af79af746d2374f4afc
isido <shido322@snu.ac.kr>
2019-07-10 01:09:08 +0900

    메모장 구현 각 버튼 별, 로직 연결

--- app/src/main/java/e/isido/memo/MainActivity.java
+++ app/src/main/java/e/isido/memo/MainActivity.java
@@ -2,29 +2,72 @@ package e.isido.memo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+//
+//    // Used to load the 'native-lib' library on application startup.
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Example of a call to a native method
+//      //  TextView tv = (TextView) findViewById(R.id.sample_text);
+//      //  tv.setText(stringFromJNI());
+//    }
+//
+//    /**
+//     * A native method that is implemented by the 'native-lib' native library,
+//     * which is packaged with this application.
+//     */
+//    public native String stringFromJNI();
+//    /* simple test commit*/
+    EditText m_MemoEdit = null;
+    TextFileManager m_TextFileManager = new TextFileManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m_MemoEdit = (EditText) findViewById(R.id.memo_edit);
 
-        // Example of a call to a native method
-      //  TextView tv = (TextView) findViewById(R.id.sample_text);
-      //  tv.setText(stringFromJNI());
+    }
+    public void onClick(View v){
+        switch(v.getId()){
+            //load text
+            case R.id.load_btn: {
+                String memoData = m_TextFileManager.load();
+                m_MemoEdit.setText(memoData);
+
+                Toast.makeText(this, "불러오기 완료", LENGTH_LONG).show();
+                break;
+            }
+            //save text
+            case R.id.save_btn: {
+                String memoData = m_MemoEdit.getText().toString();
+                m_TextFileManager.save(memoData);
+                m_MemoEdit.setText("");
+                Toast.makeText(this, "저장 완료", LENGTH_LONG).show();
+                break;
+            }
+
+            //delete text
+            case R.id.delete_btn: {
+                m_TextFileManager.delete();
+                m_MemoEdit.setText("");
+                Toast.makeText(this, "삭제 완료", LENGTH_LONG).show();
+            }
+        }
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-    /* simple test commit*/
 }
--- app/src/main/java/e/isido/memo/TextFileManager.java
+++ app/src/main/java/e/isido/memo/TextFileManager.java
@@ -9,9 +9,9 @@ import java.io.IOException;
 public class TextFileManager {
     private static final String FILE_NAME="Memo.txt";
     //메모 내용을 저장할 임시 파일
-    Context mContext = null;
+    Context m_Context = null;
     public TextFileManager(Context context) {
-        mContext = context;
+        m_Context = context;
     }
     //파일에 메모 저장하는 로직
     public void save(String strData){
@@ -23,7 +23,7 @@ public class TextFileManager {
 
         try {
             //파일에 데이터를 쓰기 위해 output 스트림 생성
-            fosMemo = mContext.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+            fosMemo = m_Context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
             //파일에 메모 적기
 
             fosMemo.write(strData.getBytes());
@@ -36,7 +36,7 @@ public class TextFileManager {
     public String load() {
         try {
             //파일에서 데이터를 읽기 위해 input 스트림 구현
-            FileInputStream fisMemo = mContext.openFileInput(FILE_NAME);
+            FileInputStream fisMemo = m_Context.openFileInput(FILE_NAME);
 
             //데이터를 읽어 온 뒤, String 타입 객체로 반환
             byte[] memoData = new byte[fisMemo.available()];
@@ -49,7 +49,7 @@ public class TextFileManager {
     }
 
     public void delete() {
-        mContext.deleteFile(FILE_NAME);
+        m_Context.deleteFile(FILE_NAME);
     }
 
 }
